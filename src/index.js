54d1672b48bc20b1312759d1a5776021340c6e25
[Jaiendiran M] <[jaiendiran2494@gmail.com]>
2021-12-22 15:43:46 +0530

    Add App component in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import { render } from '@testing-library/react';
+import react from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+// Render the App component on the page
+ReactDOM.render(<App />, document.getElementById('root'));
