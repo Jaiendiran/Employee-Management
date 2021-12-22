07f4216183d70ca5e20a11638982f60738f67672
[Jaiendiran M] <[jaiendiran2494@gmail.com]>
2021-12-22 20:28:21 +0530

    Created Seperate app component.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import { render } from '@testing-library/react';
-import react from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-}
+
 
 // Render the App component on the page
 ReactDOM.render(<App />, document.getElementById('root'));
