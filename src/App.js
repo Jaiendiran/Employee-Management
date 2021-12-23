e9954f85b4a2e8e35fda5e0e1b20a98bbf4c0905
[Jaiendiran M] <[jaiendiran2494@gmail.com]>
2021-12-23 16:52:49 +0530

    Added components to show data's in tabular format.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import Table from './Table';
 
 class App extends Component {
     render() {
+        const characters = [
+            {
+                name: 'Gandalf',
+                job: 'Wizard'
+            },
+            {
+                name: 'Legolas',
+                job: 'Elf'
+            },
+            {
+                name: 'Aragorn',
+                job: 'Action Hero'
+            }
+        ];
+
         return(
             <div className='App'>
-                <h1>Hello React!</h1>
+                <Table characterData={characters}/>
             </div>
         )
     }
